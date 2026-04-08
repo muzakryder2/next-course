5d996d5168d3d9d20dbdaaa6b35c89bdbe180a43
Daniel Castillo <muzakryder2@msn.com>
2026-04-08 18:11:53 -0500

    Added example routing. Nest is 404 page not found 37:00

---
+++ routing-demo/src/app/blog/second/page.tsx
@@ -0,0 +1,3 @@
+export default function SecondtBlog() {
+  return <h1>Second blog post</h1>;
+}
---
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,13 @@
+export default async function ReviewDetails({
+  params,
+}: {
+  params: Promise<{ productId: string; reviewId: string }>;
+}) {
+  const { productId, reviewId } = await params;
+
+  return (
+    <h1>
+      Review {reviewId} for product {productId}
+    </h1>
+  );
+}
