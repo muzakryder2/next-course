c8997292664a9aa2d99aa342946dbca81aa36621
Daniel Castillo <muzakryder2@msn.com>
2026-04-08 22:20:03 -0500

    Added more routing features. 1:03:44

---
+++ routing-demo/src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+function BarChart() {
+  return <h1>Bar Chart</h1>;
+}
+
+export default function Dahsboard() {
+  return <h1>Dashboard</h1>;
+}
--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,15 @@
+import { notFound } from "next/navigation";
+
 export default async function ReviewDetails({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
   const { productId, reviewId } = await params;
+
+  if (parseInt(reviewId) > 1000) {
+    notFound();
+  }
 
   return (
     <h1>
